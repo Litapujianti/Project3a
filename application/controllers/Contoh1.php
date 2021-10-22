30b8063803375d5e6622b6fd4cef143fe8c59d5d
Litapujianti <litapujianti70@gmail.com>
2021-10-21 21:51:29 -0700

    Upload awal projek

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Lita Pujianti
+             Saya tinggal di daerah Cilacap
+             olah raga yang saya sukai adaalah
+             Bulutangkis";
+    }
+}
